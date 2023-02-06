22684002fb87bbd582fce86f909b0a1958fb98bc
PabloDMartinez <pdmartinez3@miners.utep.edu>
2023-02-06 12:21:38 -0700

    Added another font

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c, Chars2 = d) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -33,6 +33,10 @@ int main()
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
+    case 'd':
+      puts("You selected chars2:");
+      for(char c = 'C'; c < 'F'; c++)
+	print_char_8x12(c);
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
